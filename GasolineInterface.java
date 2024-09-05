65010666d9683c8c2887e189d5374d6852d12789
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2024-09-05 12:09:25 -0400

    typo fix
    also made interfaces more consistent

--- GasolineInterface.java
+++ GasolineInterface.java
@@ -1,9 +1,9 @@
 public interface GasolineInterface {
     
+    public double calcGasMPG();
     public void setMilesfromGas(double miles);
     public void setGallonsfromGas(double gallons);
-    public double getGasMPG();
-
+    
 }
 
 
